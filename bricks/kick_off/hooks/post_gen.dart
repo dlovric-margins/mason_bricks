c31d9d742900a834ea6a397775578d0a62a8a584
Danijel <danijellovric.margins@gmail.com>
2024-01-09 15:01:45 +0100

    service locator

--- bricks/kick_off/hooks/post_gen.dart
+++ bricks/kick_off/hooks/post_gen.dart
@@ -8,28 +8,54 @@ Future<void> run(HookContext context) async {
     'freezed_annotation',
     'flutter_bloc',
     'flutter_lints',
-  ];“
+    'get_it',
+    'injectable',
+  ];
 
   final devPackages = [
     'freezed',
     'json_serializable',
     'build_runner',
+    'injectable_generator',
+    'flutter_gen',
   ];
 
   final directory = context.vars['project_name'];
 
   for (var package in packages) {
     context.logger.info('Adding $package');
-    await Process.run('fvm', ['flutter', 'pub', 'add', package],workingDirectory: directory);
+    await Process.run('fvm', ['flutter', 'pub', 'add', package],
+        workingDirectory: directory);
   }
 
   for (var package in devPackages) {
     context.logger.info('Adding dev $package');
-    await Process.run('fvm', ['flutter', 'pub', 'add', '--dev', package],workingDirectory: directory);
+    await Process.run('fvm', ['flutter', 'pub', 'add', '--dev', package],
+        workingDirectory: directory);
   }
 
   // Run `flutter packages get` after generation.
-  await Process.run('fvm', ['flutter', 'packages', 'get'],workingDirectory: directory);
+  await Process.run(
+    'fvm',
+    [
+      'flutter',
+      'packages',
+      'get',
+    ],
+    workingDirectory: directory,
+  );
+
+  await Process.run(
+      'fvm',
+      [
+        'flutter',
+        'pub',
+        'run',
+        'build_runner',
+        'build',
+        '--delete-conflicting-outputs'
+      ],
+      workingDirectory: directory);
 
   progress.complete();
 }
